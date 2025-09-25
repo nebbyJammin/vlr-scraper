d4247f002a2ec49d9bb746f358b88b8c67d9ff25
nebbyJammin <129131132+nebbyJammin@users.noreply.github.com>
2025-09-26 01:34:24 +1000

    Support for multiple chat id's in telegram notifier

--- telegram_notify/telegram_utils.py
+++ telegram_notify/telegram_utils.py
@@ -4,20 +4,22 @@ import requests
 
 USE_TELEGRAM = os.getenv("USE_TELEGRAM", "false").lower() in ("true", "1", "yes")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+chat_id_strings = os.getenv("CHAT_IDS", "")
+CHAT_IDS = [int(x.strip()) for x in chat_id_strings.split(',') if x.strip()]
 SEND_MESSAGE_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 GET_ME_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getMe"
 
 def send_telegram_msg(message: str):
-    global CHAT_ID, SEND_MESSAGE_URL
+    global CHAT_IDS, SEND_MESSAGE_URL
 
     if not USE_TELEGRAM or not TELEGRAM_TOKEN:
         return
 
-    try:
-        requests.post(url=SEND_MESSAGE_URL, data={"chat_id": CHAT_ID, "text": message}, timeout=5)
-    except Exception as e:
-        logging.getLogger("Telegram Utils").warning("Failed to send Telegram message:", e)
+    for CHAT_ID in CHAT_IDS:
+        try:
+            requests.post(url=SEND_MESSAGE_URL, data={"chat_id": CHAT_ID, "text": message}, timeout=5)
+        except Exception as e:
+            logging.getLogger("Telegram Utils").warning("Failed to send Telegram message:", e)
 
 def test_telegram_token():
     if not USE_TELEGRAM or not TELEGRAM_TOKEN:
