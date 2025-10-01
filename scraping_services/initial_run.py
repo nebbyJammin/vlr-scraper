bf4af6663393f68655913f39c85a7d826f87a91a
nebbyJammin <129131132+nebbyJammin@users.noreply.github.com>
2025-10-01 22:59:51 +1000

    changed arg handling for --build flag to include the upper bound

--- scraping_services/initial_run.py
+++ scraping_services/initial_run.py
@@ -7,7 +7,7 @@ from logging_config import MAIN_LOGGER as LOGGER
 
 def do_initial_run(SCRAPER: VLRScraper, SCRAPE_SCHEDULER: ScrapeScheduler, series_upper: int):
     """Scans every series->event->match->team recursively. May take up to 24 hours depending on worker count + sleep time. It is recommended to have ~20 workers with average sleep time of 1s."""
-    for id in range(0,series_upper):
+    for id in range(0,series_upper + 1):
         SCRAPE_SCHEDULER.enqueue_task(
             ScraperTask(
                 task_type=ScraperTaskType.SCRAPE_SERIES,
