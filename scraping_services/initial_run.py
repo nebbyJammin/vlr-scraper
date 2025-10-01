604fb7cd537beff441a6c071661fa7aadce5a3ce
nebbyJammin <129131132+nebbyJammin@users.noreply.github.com>
2025-10-01 22:58:43 +1000

    quickfix

--- scraping_services/initial_run.py
+++ scraping_services/initial_run.py
@@ -5,9 +5,9 @@ from scraper.scraper import VLRScraper
 from scraper.scraper_utils import VLRScraperOptions
 from logging_config import MAIN_LOGGER as LOGGER
 
-def do_initial_run(SCRAPER: VLRScraper, SCRAPE_SCHEDULER: ScrapeScheduler):
+def do_initial_run(SCRAPER: VLRScraper, SCRAPE_SCHEDULER: ScrapeScheduler, series_upper: int):
     """Scans every series->event->match->team recursively. May take up to 24 hours depending on worker count + sleep time. It is recommended to have ~20 workers with average sleep time of 1s."""
-    for id in range(0,85):
+    for id in range(0,series_upper):
         SCRAPE_SCHEDULER.enqueue_task(
             ScraperTask(
                 task_type=ScraperTaskType.SCRAPE_SERIES,
