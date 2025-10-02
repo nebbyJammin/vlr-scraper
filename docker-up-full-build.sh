0f8ba0d4f5c64bc929026138dfd94888f9d6e0ef
nebby <bennguyen8180@gmail.com>
2025-10-02 18:23:40 +1000

    fixed typo

--- docker-up-full-build.sh
+++ docker-up-full-build.sh
@@ -1,1 +1,1 @@
-gocker compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.build.yml up -d --build
+docker compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.build.yml up -d --build
